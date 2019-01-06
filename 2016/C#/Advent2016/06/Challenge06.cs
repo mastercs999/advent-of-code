31fd48f3fe125cc3cf02acb24f42f5542f3fdd06
Unknown <mastercs999@seznam.cz>
2019-01-06 19:50:20 +0100

    Challenge 6

---
+++ 2016/C#/Advent2016/06/Challenge06.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent2016._06
+{
+    public static class Challenge06
+    {
+        public static void Run()
+        {
+            // Load words
+            List<string> words = File.ReadAllLines(Path.Combine("06", "input")).ToList();
+
+            // Task 1
+            string message = new string(Enumerable.Range(0, words.First().Length).Select(x => words.Select(y => y[x]).GroupBy(y => y).OrderByDescending(y => y.Count()).First().Key).ToArray());
+
+            Console.WriteLine(message);
+
+            // Task 2
+            message = new string(Enumerable.Range(0, words.First().Length).Select(x => words.Select(y => y[x]).GroupBy(y => y).OrderBy(y => y.Count()).First().Key).ToArray());
+
+            Console.WriteLine(message);
+        }
+    }
+}
--- 2016/C#/Advent2016/Program.cs
+++ 2016/C#/Advent2016/Program.cs
@@ -3,6 +3,7 @@ using Advent2016._02;
 using Advent2016._03;
 using Advent2016._04;
 using Advent2016._05;
+using Advent2016._06;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,7 +20,8 @@ namespace Advent2016
             //Challenge02.Run();
             //Challenge03.Run();
             //Challenge04.Run();
-            Challenge05.Run();
+            //Challenge05.Run();
+            Challenge06.Run();
         }
     }
 }
